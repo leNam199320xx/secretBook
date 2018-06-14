b0f736c04d44b2adbf197a3fcbc0150c148dbe6a
Nam Lê <lenam19932012@gmail.com>
2018-06-14 22:37:56 +0700

    123123

--- src/app/sb-home/sb-friends/friend.component.ts
+++ src/app/sb-home/sb-friends/friend.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { SbFriendService } from './friend.service';
 import { SbAccountService } from '../../sb-account/account.service';
+import { SbUserModel } from '../../../common/models/user.model';
 
 @Component({
     selector: 'sb-friend',
@@ -8,10 +9,13 @@ import { SbAccountService } from '../../sb-account/account.service';
     styleUrls: ['friend.scss']
 })
 export class SbFriendComponent {
+    friends: SbUserModel[];
     constructor(private friendService: SbFriendService, private accountService: SbAccountService) {
-        this.friendService.getFriends(this.accountService.me.id).subscribe(res => {
-
-        });
+        if (this.accountService.me && this.accountService.me.id) {
+            this.friendService.getFriends(this.accountService.me.id).subscribe(res => {
+                this.friends = res;
+            });
+        }
     }
 
     openChatbox() {
